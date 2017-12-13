fb8b352ec1170be51fea0505918f5854e4070089
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-13 10:12:29 +0100

    Création de l'exercice 2

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 2</title>
+  </head>
+  <body>
+    <?php
+      $number1 = 0;
+      $number2 = 12;
+      while($number1 <= 20) {
+        echo ' ' . $number1 * $number2;
+        $number1++;
+      }
+    ?>
+  </body>
+</html>
